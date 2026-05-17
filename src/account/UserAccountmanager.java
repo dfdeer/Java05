cf2864d904d645773c8eb5f5c41e75e48c997af1
junsu <kimjs296@gmail.com>
2026-05-18 00:57:06 +0900

    [Test] BankSystem Console Login

--- src/account/UserAccountmanager.java
+++ src/account/UserAccountmanager.java
@@ -2,71 +2,81 @@ package account;
 
 import java.util.ArrayList;
 
-public class UserAccountmanager {
-	
-	private ArrayList<UserAccount> userList = new ArrayList<>();	// 은행 계좌 목록 변수
-	private UserAccount currentUser = null;			// 로그인 된 현재 계좌
-	
-	// userId 중복 확인
-	public boolean isUserIdDuplicated(String inUserId){
-			for (UserAccount user : userList) {
-				if (user.getUserId() == inUserId) return true;
-			}
-			return false;
-		}
-		
-	// phoneNumber 중복 확인
-	public boolean isPhoneNumberDuplicated(String inPhoneNumber) {
-		for (UserAccount user : userList) {
-			if (user.getPhoneNumber() == inPhoneNumber) return true;
-		}
-		return false;
-	}
-	
-	// 계정 생성
-	public boolean createUserAccount(String inUserId, String inPassword, String inName, String inPhoneNumber) {
-		if (isUserIdDuplicated(inUserId)) return false;
-		if (isPhoneNumberDuplicated(inPhoneNumber)) return false;
-		
-		UserAccount user = new UserAccount(inUserId, inPassword, inName, inPhoneNumber);
-		
-		userList.add(user);
-		
-		return true;
-	}
-	
-	// 계정 제거
-	public void deleteUserAccount() {
-			userList.remove(currentUser);
-		}
-		
-	// 계정 수정 (비밀번호, 이름, 전화번호)
-	public void editPassword(String newPassword) {
-		currentUser.setPassword(newPassword);
-		return;
-	}
-	public void editName(String newName) {
-		currentUser.setName(newName);
-		return;
-	}
-	public void editPhoneNumber(String newPhoneNumber) {
-		currentUser.setPhoneNumber(newPhoneNumber);
-		return;
-	}
-	
-	// 로그인
-	public boolean login(String inUserId, String inPassword) {
-		for (UserAccount user : userList) {
-			if (user.getUserId() == inUserId && user.getPassword() == inPassword) {
-				currentUser = user;
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	// 로그아웃
-	public void logout() {
-		currentUser = null;
-	}
+public class UserAccountManager {
+
+    private ArrayList<UserAccount> userList = new ArrayList<>();	// 은행 계좌 목록 변수
+    private UserAccount currentUser = null;			// 로그인 된 현재 계좌
+
+    // userId 중복 확인
+    public boolean isUserIdDuplicated(String inUserId) {
+        for (UserAccount user : userList) {
+            if (user.getUserId().equals(inUserId)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // phoneNumber 중복 확인
+    public boolean isPhoneNumberDuplicated(String inPhoneNumber) {
+        for (UserAccount user : userList) {
+            if (user.getPhoneNumber().equals(inPhoneNumber)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 계정 생성
+    public boolean createUserAccount(String inUserId, String inPassword, String inName, String inPhoneNumber) {
+        if (isUserIdDuplicated(inUserId)) {
+            return false;
+        }
+        if (isPhoneNumberDuplicated(inPhoneNumber)) {
+            return false;
+        }
+
+        UserAccount user = new UserAccount(inUserId, inPassword, inName, inPhoneNumber);
+
+        userList.add(user);
+
+        return true;
+    }
+
+    // 계정 제거
+    public void deleteUserAccount() {
+        userList.remove(currentUser);
+    }
+
+    // 계정 수정 (비밀번호, 이름, 전화번호)
+    public void editPassword(String newPassword) {
+        currentUser.setPassword(newPassword);
+        return;
+    }
+
+    public void editName(String newName) {
+        currentUser.setName(newName);
+        return;
+    }
+
+    public void editPhoneNumber(String newPhoneNumber) {
+        currentUser.setPhoneNumber(newPhoneNumber);
+        return;
+    }
+
+    // 로그인
+    public boolean login(String inUserId, String inPassword) {
+        for (UserAccount user : userList) {
+            if (user.getUserId().equals(inUserId) && user.getPassword().equals(inPassword)) {
+                currentUser = user;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 로그아웃
+    public void logout() {
+        currentUser = null;
+    }
 }
--- src/main/BankSystem.java
+++ src/main/BankSystem.java
@@ -1,27 +1,72 @@
 package main;
 
-import ui.*;
+import account.*;
+import java.util.Scanner;
 
 public class BankSystem {
+
+    private static UserAccountManager UAM = new UserAccountManager();
 
     public static void run() {
         System.out.println("은행 시스템이 실행되었습니다.");
 
-        new MainScreen();
+        UAM.createUserAccount("admin", "1111", "익명", "01011111111");
 
+        /**
+         * *new MainScreen();
+         *
+         *          *while (true) { if (signSystem.login) { System.out.println("로그인
+         * 성공!"); // 로그인 성공 후 로직 추가 break; } else { //System.out.println("로그인
+         * 실패!"); try { Thread.sleep(1000); // 1초 마다 로그인 상태 확인 } catch
+         * (InterruptedException e) { e.printStackTrace(); } } }**
+         */
+        Scanner sc = new Scanner(System.in);
+
+        printLoginMain();
+
+        OUTER:
         while (true) {
-            if (signSystem.login) {
-                System.out.println("로그인 성공!");
-				// 로그인 성공 후 로직 추가
-				break;
-            } else {
-                //System.out.println("로그인 실패!");
-				try {
-					Thread.sleep(1000); // 1초 마다 로그인 상태 확인
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("로그인을 선택하셨습니다.");
+                    System.out.println("-------------------------------");
+                    System.out.print("아이디: ");
+                    String userId = sc.next();
+                    System.out.print("비밀번호: ");
+                    String password = sc.next();
+                    System.out.println("-------------------------------");
+                    if (UAM.login(userId, password)) {
+                        System.out.println("로그인 성공!");
+                        System.out.println("-------------------------------");
+                        // 로그인 성공 후 로직 추가
+                        break OUTER;
+                    } else {
+                        System.out.println("로그인 실패! 아이디 또는 비밀번호가 잘못되었습니다.");
+                        System.out.println("-------------------------------");
+                        printLoginMain();
+                    }
+                    // 로그인 로직 추가
+                    break;
+                case 2:
+                    System.out.println("회원가입을 선택하셨습니다.");
+                    System.out.println("-------------------------------");
+                    // 회원가입 로직 추가
+                    break OUTER;
+                default:
+                    System.out.println("-------------------------------");
+                    System.out.println("잘못된 선택입니다.");
+                    System.out.println("-------------------------------");
+                    printLoginMain();
+                    break;
             }
         }
+    }
+
+    public static void printLoginMain() {
+        System.out.println("메인 메뉴:");
+        System.out.println("1. 로그인");
+        System.out.println("2. 회원가입");
+        System.out.print("선택: ");
     }
 }
