ee79498bcc480a8511ea1943ed0aec2ddcf9d1b3
bkrcrew24 <qocksdud1298@gmail.com>
2026-05-18 16:51:07 +0900

    BankAccount & Transaction 코드 수정 및 추가
    BankAccount.java 내 수정 사항
    
    - balance를 int형에서 long형으로 변경(int -> long)
    - addBalance 함수 추가.
    
    Transaction.java
    - 입금 성공 시 거래 내역 기록을 위한 코드

--- src/transaction/Transaction.java
+++ src/transaction/Transaction.java
@@ -1,7 +1,23 @@
 package transaction;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Transaction {
 	private String transactionType;	// 거래 종류 (출금, 입금, 송금 등)
-	private int amount;				// 거래량
-	private int date;				// 거래일시
+	private long amount;	// 거래 금액
+	private LocalDateTime transactionDate;	// 거래일시
+	private long AfterBalance; // 거래 후 잔액
+
+	public Transaction(String transactionType, long amount, long AfterBalance) {
+		this.transactionDate = LocalDateTime.now();
+		this.transactionType = transactionType;
+		this.amount = amount;
+		this.AfterBalance = AfterBalance;
+	}
+
+	public String toString() {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("2026-MM-dd HH:mm:ss");
+		return String.format("[s] %s |금액: %,d원 | 잔액: %,d", transactionDate.format(formatter), transactionType, amount, AfterBalance);
+	}
 }
